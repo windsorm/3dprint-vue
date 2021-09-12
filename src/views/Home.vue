f0b301c1f0a3d173d0ea634432bbd5939c32399c
Martin Windsor <martinwindsor1@gmail.com>
2021-09-12 20:59:08 +0200

    test-tailwind

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <Button /><br>
+   <Button />
+    <Button />
+     <Button />
+     <HeroSection />
+</template>
+
+<script>
+// @ is an alias to /src
+import Button from "@/components/Button.vue"
+import HeroSection from "@/components/HeroSection.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    Button,
+    HeroSection
+  }
+}
+
+</script>
